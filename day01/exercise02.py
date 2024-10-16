84c77ccf2c72d29f5b8bf0df454b9a06e6e343f7
Durgesh Tambe <dntambe@gmail.com>
2024-10-16 14:47:58 +0530

    day 01 completed

---
+++ day01/exercise02.py
@@ -0,0 +1,4 @@
+print("Hello Durgesh!\nHow are you?")
+print("Hello " + "Angela\nI'm Good, Thanks!")
+print("What about you?")
+print("I'm good," + " "+"Thanks!")
---
+++ day01/input.py
@@ -0,0 +1,10 @@
+# input() function: Prompt for the user
+
+#This line of code will take an input using the input() function
+#input("What is your name?")
+
+# print("Hello " + input("What is your name?") + "!")
+
+name = input("What is your name?")
+print("Hello " + name + "!")
+print("No of chars in your name:", len(name))
