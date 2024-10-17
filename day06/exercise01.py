940dc6819a1b7f20fe5580c804ae576125e1a926
Durgesh Tambe <dntambe@gmail.com>
2024-10-17 21:21:14 +0530

    day 06 completed

---
+++ day06/exercise01.py
@@ -0,0 +1,11 @@
+# Built-in-functions
+name = "Durgesh"
+print(f"Hello {name}!")
+
+word_length = len(name)
+print(word_length)
+
+# User-defined-functions
+def greet():
+    print(f"Hello {name}!")
+greet()
---
+++ day06/while_loop.py
@@ -0,0 +1,6 @@
+count = 0
+while count < 5:
+    count += 1
+    if count == 3:
+        continue  # Skip printing when count is 3
+    print(count)
